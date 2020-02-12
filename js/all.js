7662e9e89def43ddd9789c328c4e0fcdecffd6d7
Eric Fei <guanwha@gmail.com>
2020-02-12 12:46:32 +0800

    Implemented user's location
    1. add getUserLocation(), extractLatLong()
    2. add myMarker

--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
 import * as map from "./map.js";
 
 map.initMap();
+map.getUserLocation();
--- js/map.js
+++ js/map.js
@@ -1,7 +1,10 @@
-// initialize the map on the "map" div with a given center and zoom
+// variables
 let myLocation = [25.047794, 121.516947];   // default location
+let myMarker = null;                        // my location marker
 let map;
 
+
+// initialize the map on the "map" div with a given center and zoom
 export const initMap = () => {
   // create a map
   map = L.map('map', {
@@ -14,3 +17,27 @@ export const initMap = () => {
     attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
   }).addTo(map);
 };
+
+// get the location (asynchronous)
+export const getUserLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(extractLatLong);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+const extractLatLong = (position) => {
+  myLocation = [position.coords.latitude, position.coords.longitude];
+
+  // create/update my location marker
+  if (!myMarker) {
+    myMarker = L.marker(myLocation).addTo(map).bindPopup('You\'re here').openPopup();
+  }
+  else {
+    myMarker.setLatLng(myLocation);
+  }
+
+  // set map center
+  map.setView(myLocation);
+}
+
