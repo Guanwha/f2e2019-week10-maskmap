1c5dc2891e2ca31d438fdb0f188d2e35cf484d42
Eric Fei <guanwha@gmail.com>
2020-02-12 22:36:09 +0800

    Customized markers
    1. customize icon
       - add gpsIcon, adultIcon, childIcon, bothIcon, noneIcon
       - custom in extractPharmacies()
    
    2. others
       - add nMaskAdult, nMaskChild
       - add genPopup() to customize popup

--- js/map.js
+++ js/map.js
@@ -3,6 +3,55 @@ let myLocation = [25.047794, 121.516947];   // default location
 let myMarker = null;                        // my location marker
 let map;
 let markers = null;                         // MarkerClusterGroup
+let nMaskAdult = 0;
+let nMaskChild = 0;
+
+// icons
+let gpsIcon = new L.Icon({
+  iconUrl: '/images/map_marker_me.svg',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 43],
+  iconAnchor: [16, 43],
+  shadowSize: [43, 43],
+  shadowAnchor: [14, 43],
+  popupAnchor: [0, -43],
+});
+let adultIcon = new L.Icon({
+  iconUrl: '/images/map_marker_adult.svg',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 43],
+  iconAnchor: [16, 43],
+  shadowSize: [43, 43],
+  shadowAnchor: [14, 43],
+  popupAnchor: [0, -43],
+});
+let childIcon = new L.Icon({
+  iconUrl: '/images/map_marker_child.svg',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 43],
+  iconAnchor: [16, 43],
+  shadowSize: [43, 43],
+  shadowAnchor: [14, 43],
+  popupAnchor: [0, -43],
+});
+let bothIcon = new L.Icon({
+  iconUrl: '/images/map_marker_both.svg',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 43],
+  iconAnchor: [16, 43],
+  shadowSize: [43, 43],
+  shadowAnchor: [14, 43],
+  popupAnchor: [0, -43],
+});
+let noneIcon = new L.Icon({
+  iconUrl: '/images/map_marker_none.svg',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [32, 43],
+  iconAnchor: [16, 43],
+  shadowSize: [43, 43],
+  shadowAnchor: [14, 43],
+  popupAnchor: [0, -43],
+});
 
 
 // initialize the map on the "map" div with a given center and zoom
@@ -35,15 +84,6 @@ const extractLatLong = (position) => {
 
   // create/update my location marker
   if (!myMarker) {
-    var gpsIcon = new L.Icon({
-      iconUrl: '/images/map_marker_me.svg',
-      shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-      iconSize: [32, 43],
-      iconAnchor: [16, 43],
-      shadowSize: [43, 43],
-      shadowAnchor: [14, 43],
-      popupAnchor: [0, -43],
-    });
     myMarker = L.marker(myLocation, { icon: gpsIcon }).addTo(map).bindPopup('You\'re here').openPopup();
   }
   else {
@@ -57,10 +97,42 @@ const extractLatLong = (position) => {
 // extract pharmacies from api
 export const extractPharmacies = (responseText) => {
   let data = JSON.parse(responseText).features;
+  nMaskAdult = 0;
+  nMaskChild = 0;
   for (let i=0; i<data.length; i++) {
-    markers.addLayer(L.marker([data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]])
-                      .bindPopup(data[i].properties.name));
+    // decide icon type
+    let icon = null;
+    if (data[i].properties.mask_adult == 0 && data[i].properties.mask_child == 0) {
+      icon = noneIcon;
+    }
+    else if (data[i].properties.mask_adult == 0) {
+      icon = childIcon;
+    }
+    else if (data[i].properties.mask_child == 0) {
+      icon = adultIcon;
+    }
+    else {
+      icon = bothIcon;
+    }
+
+    // create marker
+    markers.addLayer(L.marker([data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]], { icon: icon })
+                      .bindPopup(genPopup(data[i].properties)));
+
+    // statistic
+    nMaskAdult += data[i].properties.mask_adult;
+    nMaskChild += data[i].properties.mask_child;
   }
   map.addLayer(markers);
+  console.log(`成人口罩剩餘：${nMaskAdult}。兒童口罩剩餘：${nMaskChild}。`)
+}
+
+// popup layout
+const genPopup = (property) => {
+  return `
+  <h1>${property.name}</h1>
+  <div>成人：${property.mask_adult}</div>
+  <div>兒童：${property.mask_child}</div>
+  `
 }
 
