d475bc6dc7fa2671bda48b4e8a4f09ee237cc1e8
Eric Fei <guanwha@gmail.com>
2020-02-15 00:52:26 +0800

    Added Bootstrap into project
    1. Unified nav to menu

--- js/ui.js
+++ js/ui.js
@@ -1,31 +1,31 @@
 
 // menu open/close
-let elNavBar = document.querySelector('.menu');
+let elMenu = document.querySelector('.menu');
 let elBtnMenuCtrl = document.querySelector('.btn-menu-ctrl');
 let elBtnMenuCtrlIcon = document.querySelector('.btn-menu-ctrl .icon');
 
-elNavBar.dataset.isopen = 0;
+elMenu.dataset.isopen = 0;
 elBtnMenuCtrl.addEventListener('click', () => {
-  if (elNavBar.dataset.isopen == 1) {
+  if (elMenu.dataset.isopen == 1) {
     // close menu
-    elNavBar.style.width = 0;
-    elNavBar.dataset.isopen = 0;
+    elMenu.style.width = 0;
+    elMenu.dataset.isopen = 0;
     elBtnMenuCtrlIcon.style.transform = "rotate(180deg)";
   }
   else {
     // open menu
-    elNavBar.style.width = '90%';
-    elNavBar.dataset.isopen = 1;
+    elMenu.style.width = '90%';
+    elMenu.dataset.isopen = 1;
     elBtnMenuCtrlIcon.style.transform = "rotate(0deg)";
   }
 });
 
 let elMap = document.getElementById('map');
 elMap.addEventListener('click', () => {
-  if (elNavBar.dataset.isopen == 1) {
+  if (elMenu.dataset.isopen == 1) {
     // close menu
-    elNavBar.style.width = 0;
-    elNavBar.dataset.isopen = 0;
+    elMenu.style.width = 0;
+    elMenu.dataset.isopen = 0;
     elBtnMenuCtrlIcon.style.transform = "rotate(180deg)";
   }
 });
